74aa405ea0fa175ba19d002ccbc67622c6bb0811
wangfanghua <wangfanghua@jk.pingan-inc.cn>
2017-07-17 19:22:46 +0800

    组件项目模板环境配置完成

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+// libs
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+interface IProps {
+}
+
+function App(props: IProps) {
+    return <div>
+        App
+    </div>
+}
+
+ReactDOM.render(<App />, document.getElementById("main"), null);
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+// node libs
+const path = require('path');
+const webpack = require('webpack');
+
+// plugins
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// path
+let sourcePath = path.join(__dirname, 'src');
+let buildPath = path.join(__dirname, 'dist');
+
+let env = process.env.NODE_ENV === "development" ? "development" : "production";
+
+if (env === "development") {
+    buildPath = path.join(__dirname, "static");
+}
+
+module.exports = {
+    entry: path.join(sourcePath, "main.tsx"),
+    output: {
+        path: buildPath,
+        filename: "bundles/[name].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                include: [sourcePath],
+                use: ["awesome-typescript-loader"]
+            },
+            {
+                test: /\.less$/,
+                include: [sourcePath],
+                use: ExtractTextWebpackPlugin.extract({
+                    fallback: "style-loader",
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                            }
+                        },
+                        'less-loader'
+                    ]
+                })
+            },
+            {
+                test: /\.(jpeg|jpg|png|gif)/,
+                include: [path.join(sourcePath, "common/images")],
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: "images/[name]-[hash:8].[ext]"
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.DefinePlugin({
+            "process.env": JSON.stringify(env)
+        }),
+        new HtmlWebpackPlugin({
+            title: "AsyncDemo-Monkey",
+            template: path.join(sourcePath, "index.html")
+        }),
+        new ExtractTextWebpackPlugin("css/index.css")
+    ],
+    resolve: {
+        modules: [
+            "node_modules",
+            sourcePath
+        ],
+        extensions: [".tsx", ".ts", ".jsx", ".js", ".less", ".css", ".json"]
+    },
+    target: "web",
+    devServer: {
+        contentBase: buildPath,
+        host: "0.0.0.0",
+        port: 8080,
+        compress: true,
+        watchContentBase: true,
+        hot: true,
+        noInfo: true,
+        quiet: true,
+        proxy: {
+            "/v1": "http:localhost:9000"
+        }
+    }
+};
