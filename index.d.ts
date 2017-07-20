340c4979337d47844ca1f7268f54d62a9c86f452
wangfanghua <wangfanghua@jk.pingan-inc.cn>
2017-07-20 09:09:21 +0800

    类型文件

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,11 @@
-declare class Banner {
-    static defultProps: any;
+declare module "Banner" {
+    export class Banner {
+        constructor(props: any);
+        setState(state: any);
+        forceUpdate();
+        refs: any;
+        state: any;
+        props: any;
+        context: any;
+    }
 }
