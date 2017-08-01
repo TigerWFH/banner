47c85f3eef55c3bb7e9426521d1d7f8f05c992b8
wangfanghua <wangfanghua@jk.cn>
2017-08-01 11:12:10 +0800

    1、配置babel环境 2、测试demo完成

--- test/banner.test.js
+++ test/banner.test.js
@@ -1,11 +1,18 @@
-// var React = require('react');
-var enzyme = require('enzyme');
-
-jest.dontMock('../lib/banner');
-var Banner = require('../lib/banner');
+import React from 'react';
+import { Banner } from '../lib/banner';
+import { shallow, mount, render } from 'enzyme';
 
 describe("Banner Test", function () {
-    it("111", function () {
-        expect(true).toBe(true);
+    it("should render with throwing an error", function () {
+        expect(shallow(<Banner />).contains(<div className="sx-banner-wrapper"></div>)).not.toBe(true);
     });
+    it("should be selectable by class 'sx-banner-wrapper' ", function () {
+        expect(shallow(<Banner />).is('.sx-banner-wrapper')).toBe(true);
+    });
+    it("should mount in a fill dom", () => {
+        expect(mount(<Banner />).find('.sx-banner-wrapper').length).toBe(1);
+    });
+    it("should render to static HTML", () => {
+        expect(render(<Banner />).text()).toBe('');
+    })
 });
